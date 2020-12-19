e553c20f5445ba4e0bacbfb63b12e9fb98d5f7f8
Anuraag Basu <anuraag.1988@gmail.com>
2020-12-19 19:27:38 +0100

    new Header with link to CreateProject

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
 
 import AppBar from './styles';
 
 const NavBar: React.FC<any> = () => (
   <AppBar position="fixed">
-    <div className="section logoSection">
-      <div>Connect & Grow</div>
-    </div>
+		<Grid container className="sectionWrapper">
+			<Grid item className="section leftSection">
+				<Link to="/">Connect & Grow</Link>
+			</Grid>
+			<Grid item className="section rightSection">
+				<Link to="/create" className="addLink">Create your project</Link>
+			</Grid>
+		</Grid>
   </AppBar>
 );
 
