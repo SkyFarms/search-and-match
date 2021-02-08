01dd2f43ad3fecb0a4c17d0ace3a1f44d4d3f652
Anuraag Basu <anuraag.1988@gmail.com>
2021-02-08 16:01:15 +0100

    responsive styles; remvoing redux stuff

--- src/pages/LandingPage/Section.tsx
+++ src/pages/LandingPage/Section.tsx
@@ -14,7 +14,7 @@ const Section: React.FC<Props> = ({ title, className = '', items }) => {
 	return (
 		<SectionContainer className={`section ${className}`}>
 			<div className="sectionContent">
-				<Typography variant="h3" className="title">{title}</Typography>
+				<Typography variant="h4" className="title">{title}</Typography>
 				<GridLayout items={items} />
 			</div>
 		</SectionContainer>
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,5 +1,5 @@
 import red from "@material-ui/core/colors/red";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
 // A custom theme for this app
 const theme = createMuiTheme({
@@ -19,4 +19,4 @@ const theme = createMuiTheme({
   },
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
